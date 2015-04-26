beaa85833413f0bdb4a3aa2fcb44e130f85307c2
baal <baal@belightsoft.com>
2015-04-26 10:33:02 +0300

    Добавлен файл для сниппетов js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function convertForUIListToArray () {
+    
+
+    var htmlstring = '<ul><li>Password can not be blank</li><li>Password cannot be less than 8 symbols</li><li>Password should contain at least one lower case letter</li><li>Password should contain at least one upper case letter</li><li>Password should contain at least one digit</li></ul>';
+              
+    var arr = [];
+
+    $(htmlstring).children().each(function(_, node) {
+         arr.push(node.textContent);
+    });
+
+    console.log("!!!!!!");
+    console.log(arr);
+
+}
