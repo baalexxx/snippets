71fa1c9891794894575578ccf45136ef490b9287
baal <baal@belightsoft.com>
2015-04-26 10:50:00 +0300

    Added links

--- javascript.js
+++ javascript.js
@@ -12,5 +12,14 @@ function convertForUIListToArray () {
     console.log("!!!!!!");
     console.log("!!!!!!!!");
     console.log(arr);
+    
+    // http://encosia.com/use-jquery-to-extract-data-from-html-lists-and-tables/
+    // http://javascript.ru/forum/jquery/28620-najjti-kolichestvo-ehlementov-spiska.html
+    // var res = html.map(function() {
+    //   // For each <li> in the list, return its inner text and let .map()
+    //   //  build an array of those values.
+    //   return $(this).text();
+    // }).get();
+    
 
 }
