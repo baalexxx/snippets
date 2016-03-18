9af465abe84e3e97b10632633536e82910e22815
Alex Baev <ab@gamayun.sk>
2016-03-18 15:03:46 +0100

    Added script 'Edit an incorrect commit message in Git'

---
+++ Snippets/Script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  Script.sh
+#  Snippets
+#
+#  Created by Oleksii Baiev on 18/03/16.
+#  Copyright © 2016 baevsoft. All rights reserved.
+
+
+# Edit an incorrect commit message in Git
+git commit --amend -m "New commit message"
